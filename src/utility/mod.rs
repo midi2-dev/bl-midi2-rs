040183a65c88aec8308ea68b3a3347923dec3d1a
Ben Leadbetter - Mac2 <ben.leadbetter@native-instruments.com>
2022-09-28 23:25:37 +0200

    remove mid level mesage type

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,5 +1,15 @@
+use crate::{
+    error::Error,
+    packet::Packet,
+};
+
 pub mod time_stamp;
 pub mod no_op;
-pub mod message;
 
-pub use message::*;
+pub fn validate_packet(p: &Packet) -> Result<(), Error> {
+    if p.nibble(0) != ux::u4::new(0x0) {
+        Err(Error::InvalidData)
+    } else {
+        Ok(())
+    }
+}
