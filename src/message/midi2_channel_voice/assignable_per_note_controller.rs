2c59b7f4b44feab84b791d233577cfa18e2af17c
Ben Leadbetter - Mac2 <ben.leadbetter@native-instruments.com>
2022-10-13 16:55:29 +0200

    implement assignable per note midi2 message

--- src/message/midi2_channel_voice/assignable_per_note_controller.rs
+++ src/message/midi2_channel_voice/assignable_per_note_controller.rs
@@ -13,12 +13,13 @@ pub struct Message {
     group: ux::u4,
     channel: ux::u4,
     note: ux::u7,
+    index: u8,
     data: u32,
 }
 
 impl Message {
     pub const TYPE_CODE: ux::u4 = super::TYPE_CODE;
-    pub const OP_CODE: ux::u4 = ux::u4::new(0b1010);
+    pub const OP_CODE: ux::u4 = ux::u4::new(0b0001);
 }
 
 impl std::convert::TryFrom<Packet> for Message {
@@ -29,6 +30,7 @@ impl std::convert::TryFrom<Packet> for Message {
             group: super::group_from_packet(&p),
             channel: super::channel_from_packet(&p),
             note: truncate(p.octet(2)),
+            index: p.octet(3),
             data: p[1],
         })
     }
@@ -39,6 +41,7 @@ impl From<Message> for Packet {
         let mut p = Packet::new();
         super::write_data_to_packet(m.group, Message::OP_CODE, m.channel, &mut p);
         p.set_octet(2, m.note.into());
+        p.set_octet(3, m.index);
         p[1] = m.data;
         p
     }
@@ -50,9 +53,29 @@ mod tests {
 
     #[test]
     fn deserialize() {
+        assert_eq!(
+            Message::try_from(Packet::from_data(&[0x4410_2A05, 0x3691_2151])),
+            Ok(Message {
+                group: ux::u4::new(0x4),
+                channel: ux::u4::new(0x0),
+                note: ux::u7::new(0x2A),
+                index: 0x05,
+                data: 0x3691_2151,
+            }),
+        );
     }
 
     #[test]
     fn serialize() {
+        assert_eq!(
+            Packet::from(Message {
+                group: ux::u4::new(0x8),
+                channel: ux::u4::new(0xE),
+                note: ux::u7::new(0x1A),
+                index: 0xF6,
+                data: 0xBE90_8008,
+            }),
+            Packet::from_data(&[0x481E_1AF6, 0xBE90_8008]),
+        );
     }
 }
