56ffe615d1d5fe2c5152a5b9c8c17c509f7c4ab0
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2023-09-01 14:54:40 +0200

    remove deprecated ci messages

--- src/ci/helpers.rs
+++ src/ci/helpers.rs
@@ -1,10 +1,10 @@
 use crate::{
-    ci::{DeviceId, Protocol},
+    ci::DeviceId,
     error::Error,
     message::system_exclusive_7bit as sysex7,
     message::system_exclusive_8bit as sysex8,
     result::Result,
-    util::{BitOps, Encode7Bit, Truncate},
+    util::{Encode7Bit, Truncate},
 };
 
 pub struct StandardDataIterator {
@@ -144,99 +144,6 @@ impl core::iter::Iterator for ProtocolDataIterator {
     }
 }
 
-pub fn protocol_data(protocol: &Protocol) -> ProtocolDataIterator {
-    match protocol {
-        Protocol::Midi1 {
-            size_of_packet_extension,
-            jitter_reduction_extension,
-            version,
-        } => {
-            ProtocolDataIterator {
-                buffer: [
-                    0x1,
-                    (*version).into(),
-                    *0x0_u8
-                        .set_bit(6, *size_of_packet_extension)
-                        .set_bit(7, *jitter_reduction_extension),
-                    0x0,
-                    0x0, // reserved
-                    0x0,
-                    0x0,
-                    0x0, // padding
-                ],
-                i: 0,
-            }
-        }
-        Protocol::Midi2 {
-            jitter_reduction_extension,
-            version,
-        } => {
-            ProtocolDataIterator {
-                buffer: [
-                    0x2,
-                    (*version).into(),
-                    *0x0_u8.set_bit(7, *jitter_reduction_extension),
-                    0x0,
-                    0x0, // reserved
-                    0x0,
-                    0x0,
-                    0x0, // padding
-                ],
-                i: 0,
-            }
-        }
-    }
-}
-
-pub fn read_protocol<Byte: core::convert::Into<u8>, I: core::iter::Iterator<Item = Byte>>(
-    mut data: I,
-) -> Result<Protocol> {
-    let ty: u8 = data.next().ok_or(Error::BufferOverflow)?.into();
-    let version: u8 = data.next().ok_or(Error::BufferOverflow)?.into();
-    let flags: u8 = data.next().ok_or(Error::BufferOverflow)?.into();
-
-    // reserved data
-    data.next().ok_or(Error::BufferOverflow)?;
-    data.next().ok_or(Error::BufferOverflow)?;
-
-    match ty {
-        0x1 => Ok(Protocol::Midi1 {
-            size_of_packet_extension: flags.bit(6),
-            jitter_reduction_extension: flags.bit(7),
-            version: version.truncate(),
-        }),
-        0x2 => Ok(Protocol::Midi2 {
-            jitter_reduction_extension: flags.bit(7),
-            version: version.truncate(),
-        }),
-        _ => Err(Error::InvalidData),
-    }
-}
-
-pub fn validate_protocol_data<I: core::iter::Iterator<Item = u8>>(mut data: I) -> Result<()> {
-    if let Some(v) = data.next() {
-        if ![1u8, 2u8].iter().any(|&code| code == v) {
-            return Err(Error::InvalidData);
-        }
-    }
-    // todo: version assertion?
-    if data.nth(3).is_some() {
-        Ok(())
-    } else {
-        Err(Error::InvalidData)
-    }
-}
-
-pub fn source_from_payload<I: core::iter::Iterator<Item = u8>>(mut payload: I) -> ux::u28 {
-    payload.nth(4);
-    ux::u28::from_u7s(&[
-        payload.next().unwrap(),
-        payload.next().unwrap(),
-        payload.next().unwrap(),
-        payload.next().unwrap(),
-    ])
-}
-
 pub fn destination_from_payload<I: core::iter::Iterator<Item = u8>>(mut payload: I) -> ux::u28 {
     payload.nth(8);
     ux::u28::from_u7s(&[
@@ -245,8 +152,4 @@ pub fn destination_from_payload<I: core::iter::Iterator<Item = u8>>(mut payload:
         payload.next().unwrap(),
         payload.next().unwrap(),
     ])
-}
-
-pub fn authority_level_from_payload<I: core::iter::Iterator<Item = u8>>(mut payload: I) -> ux::u7 {
-    payload.nth(STANDARD_DATA_SIZE).unwrap().truncate()
 }
--- src/ci/mod.rs
+++ src/ci/mod.rs
@@ -1,34 +1,16 @@
-pub use confirm_protocol::ConfirmProtocolBuilder;
-pub use confirm_protocol::ConfirmProtocolMessage;
 pub use discovery::query::DiscoveryQueryBuilder;
 pub use discovery::query::DiscoveryQueryMessage;
 pub use discovery::reply::DiscoveryReplyBuilder;
 pub use discovery::reply::DiscoveryReplyMessage;
-pub use initiate_protocol_negotiation::query::InitiateProtocolNegotiationBuilder as InitiateProtocolNegotiationQueryBuilder;
-pub use initiate_protocol_negotiation::query::InitiateProtocolNegotiationMessage as InitiateProtocolNegotiationQueryMessage;
-pub use initiate_protocol_negotiation::reply::InitiateProtocolNegotiationBuilder as InitiateProtocolNegotiationReplyBuilder;
-pub use initiate_protocol_negotiation::reply::InitiateProtocolNegotiationMessage as InitiateProtocolNegotiationReplyMessage;
 pub use invalidate_muid::InvalidateMuidBuilder;
 pub use invalidate_muid::InvalidateMuidMessage;
 pub use nak::NakBuilder;
 pub use nak::NakMessage;
-pub use protocol::Protocol;
-pub use set_protocol::SetProtocolBuilder;
-pub use set_protocol::SetProtocolMessage;
-//pub use test_protocol::query::Message as TestProtocolQueryMessage;
-//pub use test_protocol::query::Builder as TestProtocolQueryMessageBuilder;
-//pub use test_protocol::reply::Message as TestProtocolReplyMessage;
-//pub use test_protocol::reply::Builder as TestProtocolReplyMessageBuilder;
 
-mod confirm_protocol;
 mod discovery;
 mod helpers;
-mod initiate_protocol_negotiation;
 mod invalidate_muid;
 mod nak;
-mod protocol;
-mod set_protocol;
-//mod test_protocol;
 
 const VERSION: u8 = 0x01;
 
