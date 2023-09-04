6e4e59afecf227e9d9fa2a0ac3a7c5d6e6746ab4
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2023-09-04 09:14:47 +0200

    only expose unsigned types from ux

--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,12 @@ mod util;
 
 pub use error::*;
 pub use result::*;
-pub use ux::*;
+
+// forward expose some numeric types from the
+// 3rd party ux crate
+#[rustfmt::skip]
+pub use ux:: {
+    u1, u2, u3, u4, u5, u6, u7, u9, u10, u11, u12,
+    u13, u14, u15, u17, u18, u19, u20, u21, u22,
+    u23, u24, u25, u26, u27, u28, u29, u30, u31,
+};
