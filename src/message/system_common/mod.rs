db7265ceb51e01894d7b3b878f45d73a25d5c9da
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2023-11-21 17:20:31 +0100

    aggregate utility and sys common messages

--- src/message/system_common/mod.rs
+++ src/message/system_common/mod.rs
@@ -1,7 +1,4 @@
-// use crate::{
-//     util::{schema::*, BitOps},
-//     *,
-// };
+use crate::{util::BitOps, *};
 
 pub const TYPE_CODE: u32 = 0x1;
 
@@ -59,127 +56,147 @@ pub use tune_request::TuneRequestBorrowed;
 pub use tune_request::TuneRequestBuilder;
 pub use tune_request::TuneRequestOwned;
 
-// pub enum SystemCommonMessage<'a, B>
-// where
-//     B: Buffer
-//         + Property<
-//             NumericalConstant<ACTIVE_SENSING>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<CONTINUE>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<RESET>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<SONG_POSITION_POINTER>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<SONG_SELECT>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<START>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<STOP>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<TIME_CODE>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<TIME_CODE>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<TIMING_CLOCK>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<
-//             NumericalConstant<TUNE_REQUEST>,
-//             UmpSchema<0x00FF_0000, 0x0, 0x0, 0x0>,
-//             BytesSchema<0xFF, 0x0, 0x0>,
-//         > + Property<NumericalConstant<TYPE_CODE>, UmpSchema<0xF000_0000, 0x0, 0x0, 0x0>, ()>
-//         + Property<u14, UmpSchema<0x0000_7F7F, 0x0, 0x0, 0x0>, BytesSchema<0x0, 0x7F, 0x7F>>
-//         + Property<u7, UmpSchema<0x0000_7F00, 0x0, 0x0, 0x0>, BytesSchema<0x0, 0x7F, 0x0>>,
-// {
-//     ActiveSensing(ActiveSensingMessage<'a, B>),
-//     Continue(ContinueMessage<'a, B>),
-//     Reset(ResetMessage<'a, B>),
-//     SongPositionPointer(SongPositionPointerMessage<'a, B>),
-//     SongSelect(SongSelectMessage<'a, B>),
-//     Start(StartMessage<'a, B>),
-//     Stop(StopMessage<'a, B>),
-//     TimeCode(TimeCodeMessage<'a, B>),
-//     TimingClock(TimingClockMessage<'a, B>),
-//     TuneRequest(TuneRequestMessage<'a, B>),
-// }
-//
-// use SystemCommonMessage::*;
-//
-// pub const ACTIVE_SENSING: u32 = 0xFE;
-// pub const CONTINUE: u32 = 0xFB;
-// pub const RESET: u32 = 0xFF;
-// pub const SONG_POSITION_POINTER: u32 = 0xF2;
-// pub const SONG_SELECT: u32 = 0xF3;
-// pub const START: u32 = 0xFA;
-// pub const STOP: u32 = 0xFC;
-// pub const TIME_CODE: u32 = 0xF1;
-// pub const TIMING_CLOCK: u32 = 0xF8;
-// pub const TUNE_REQUEST: u32 = 0xF6;
-//
-// impl<'a> Message<'a, Ump> for SystemCommonMessage<'a, Ump> {
-//     fn data(&self) -> &'a [u32] {
-//         match self {
-//             ActiveSensing(m) => m.data(),
-//             Continue(m) => m.data(),
-//             Reset(m) => m.data(),
-//             SongPositionPointer(m) => m.data(),
-//             SongSelect(m) => m.data(),
-//             Start(m) => m.data(),
-//             Stop(m) => m.data(),
-//             TimeCode(m) => m.data(),
-//             TimingClock(m) => m.data(),
-//             TuneRequest(m) => m.data(),
-//         }
-//     }
-//     fn validate_data(data: &[u32]) -> Result<()> {
-//         match data[0].octet(1).into() {
-//             ACTIVE_SENSING => ActiveSensingMessage::<Ump>::validate_data(data),
-//             CONTINUE => ContinueMessage::<Ump>::validate_data(data),
-//             RESET => ResetMessage::<Ump>::validate_data(data),
-//             SONG_POSITION_POINTER => SongPositionPointerMessage::<Ump>::validate_data(data),
-//             SONG_SELECT => SongSelectMessage::<Ump>::validate_data(data),
-//             START => StartMessage::<Ump>::validate_data(data),
-//             STOP => StopMessage::<Ump>::validate_data(data),
-//             TIME_CODE => TimeCodeMessage::<Ump>::validate_data(data),
-//             TIMING_CLOCK => TimingClockMessage::<Ump>::validate_data(data),
-//             TUNE_REQUEST => TuneRequestMessage::<Ump>::validate_data(data),
-//             _ => Err(Error::InvalidData),
-//         }
-//     }
-//     fn from_data_unchecked(data: &'a [u32]) -> Self {
-//         match data[0].octet(1).into() {
-//             ACTIVE_SENSING => ActiveSensing(ActiveSensingMessage::from_data_unchecked(data)),
-//             CONTINUE => Continue(ContinueMessage::from_data_unchecked(data)),
-//             RESET => Reset(ResetMessage::from_data_unchecked(data)),
-//             SONG_POSITION_POINTER => {
-//                 SongPositionPointer(SongPositionPointerMessage::from_data_unchecked(data))
-//             }
-//             SONG_SELECT => SongSelect(SongSelectMessage::from_data_unchecked(data)),
-//             START => Start(StartMessage::from_data_unchecked(data)),
-//             STOP => Stop(StopMessage::from_data_unchecked(data)),
-//             TIME_CODE => TimeCode(TimeCodeMessage::from_data_unchecked(data)),
-//             TIMING_CLOCK => TimingClock(TimingClockMessage::from_data_unchecked(data)),
-//             TUNE_REQUEST => TuneRequest(TuneRequestMessage::from_data_unchecked(data)),
-//             _ => panic!(),
-//         }
-//     }
-// }
+pub enum SystemCommonBorrowed<'a> {
+    ActiveSensing(ActiveSensingBorrowed<'a>),
+    Continue(ContinueBorrowed<'a>),
+    Reset(ResetBorrowed<'a>),
+    SongPositionPointer(SongPositionPointerBorrowed<'a>),
+    SongSelect(SongSelectBorrowed<'a>),
+    Start(StartBorrowed<'a>),
+    Stop(StopBorrowed<'a>),
+    TimeCode(TimeCodeBorrowed<'a>),
+    TimingClock(TimingClockBorrowed<'a>),
+    TuneRequest(TuneRequestBorrowed<'a>),
+}
+
+pub enum SystemCommonOwned {
+    ActiveSensing(ActiveSensingOwned),
+    Continue(ContinueOwned),
+    Reset(ResetOwned),
+    SongPositionPointer(SongPositionPointerOwned),
+    SongSelect(SongSelectOwned),
+    Start(StartOwned),
+    Stop(StopOwned),
+    TimeCode(TimeCodeOwned),
+    TimingClock(TimingClockOwned),
+    TuneRequest(TuneRequestOwned),
+}
+
+pub const ACTIVE_SENSING: u32 = 0xFE;
+pub const CONTINUE: u32 = 0xFB;
+pub const RESET: u32 = 0xFF;
+pub const SONG_POSITION_POINTER: u32 = 0xF2;
+pub const SONG_SELECT: u32 = 0xF3;
+pub const START: u32 = 0xFA;
+pub const STOP: u32 = 0xFC;
+pub const TIME_CODE: u32 = 0xF1;
+pub const TIMING_CLOCK: u32 = 0xF8;
+pub const TUNE_REQUEST: u32 = 0xF6;
+
+impl<'a> Data for SystemCommonBorrowed<'a> {
+    fn data(&self) -> &[u32] {
+        use SystemCommonBorrowed::*;
+        match self {
+            ActiveSensing(m) => m.data(),
+            Continue(m) => m.data(),
+            Reset(m) => m.data(),
+            SongPositionPointer(m) => m.data(),
+            SongSelect(m) => m.data(),
+            Start(m) => m.data(),
+            Stop(m) => m.data(),
+            TimeCode(m) => m.data(),
+            TimingClock(m) => m.data(),
+            TuneRequest(m) => m.data(),
+        }
+    }
+}
+
+impl Data for SystemCommonOwned {
+    fn data(&self) -> &[u32] {
+        use SystemCommonOwned::*;
+        match self {
+            ActiveSensing(m) => m.data(),
+            Continue(m) => m.data(),
+            Reset(m) => m.data(),
+            SongPositionPointer(m) => m.data(),
+            SongSelect(m) => m.data(),
+            Start(m) => m.data(),
+            Stop(m) => m.data(),
+            TimeCode(m) => m.data(),
+            TimingClock(m) => m.data(),
+            TuneRequest(m) => m.data(),
+        }
+    }
+}
+
+impl<'a> Grouped for SystemCommonBorrowed<'a> {
+    fn group(&self) -> u4 {
+        use SystemCommonBorrowed::*;
+        match self {
+            ActiveSensing(m) => m.group(),
+            Continue(m) => m.group(),
+            Reset(m) => m.group(),
+            SongPositionPointer(m) => m.group(),
+            SongSelect(m) => m.group(),
+            Start(m) => m.group(),
+            Stop(m) => m.group(),
+            TimeCode(m) => m.group(),
+            TimingClock(m) => m.group(),
+            TuneRequest(m) => m.group(),
+        }
+    }
+}
+
+impl Grouped for SystemCommonOwned {
+    fn group(&self) -> u4 {
+        use SystemCommonOwned::*;
+        match self {
+            ActiveSensing(m) => m.group(),
+            Continue(m) => m.group(),
+            Reset(m) => m.group(),
+            SongPositionPointer(m) => m.group(),
+            SongSelect(m) => m.group(),
+            Start(m) => m.group(),
+            Stop(m) => m.group(),
+            TimeCode(m) => m.group(),
+            TimingClock(m) => m.group(),
+            TuneRequest(m) => m.group(),
+        }
+    }
+}
+
+impl<'a> FromData<'a> for SystemCommonBorrowed<'a> {
+    fn validate_data(data: &[u32]) -> Result<()> {
+        match data[0].octet(1).into() {
+            ACTIVE_SENSING => ActiveSensingBorrowed::validate_data(data),
+            CONTINUE => ContinueBorrowed::validate_data(data),
+            RESET => ResetBorrowed::validate_data(data),
+            SONG_POSITION_POINTER => SongPositionPointerBorrowed::validate_data(data),
+            SONG_SELECT => SongSelectBorrowed::validate_data(data),
+            START => StartBorrowed::validate_data(data),
+            STOP => StopBorrowed::validate_data(data),
+            TIME_CODE => TimeCodeBorrowed::validate_data(data),
+            TIMING_CLOCK => TimingClockBorrowed::validate_data(data),
+            TUNE_REQUEST => TuneRequestBorrowed::validate_data(data),
+            _ => Err(Error::InvalidData),
+        }
+    }
+    fn from_data_unchecked(data: &'a [u32]) -> Self {
+        use SystemCommonBorrowed::*;
+        match data[0].octet(1).into() {
+            ACTIVE_SENSING => ActiveSensing(ActiveSensingBorrowed::from_data_unchecked(data)),
+            CONTINUE => Continue(ContinueBorrowed::from_data_unchecked(data)),
+            RESET => Reset(ResetBorrowed::from_data_unchecked(data)),
+            SONG_POSITION_POINTER => {
+                SongPositionPointer(SongPositionPointerBorrowed::from_data_unchecked(data))
+            }
+            SONG_SELECT => SongSelect(SongSelectBorrowed::from_data_unchecked(data)),
+            START => Start(StartBorrowed::from_data_unchecked(data)),
+            STOP => Stop(StopBorrowed::from_data_unchecked(data)),
+            TIME_CODE => TimeCode(TimeCodeBorrowed::from_data_unchecked(data)),
+            TIMING_CLOCK => TimingClock(TimingClockBorrowed::from_data_unchecked(data)),
+            TUNE_REQUEST => TuneRequest(TuneRequestBorrowed::from_data_unchecked(data)),
+            _ => panic!(),
+        }
+    }
+}
--- src/message/utility/mod.rs
+++ src/message/utility/mod.rs
@@ -12,38 +12,76 @@ pub use time_stamp::TimeStampBorrowed;
 pub use time_stamp::TimeStampBuilder;
 pub use time_stamp::TimeStampOwned;
 
-// pub enum UtilityMessage<'a> {
-//     NoOp(NoOpMessage<'a>),
-//     TimeStamp(TimeStampMessage<'a>),
-// }
-//
-// const NO_OP_CODE: u8 = 0b0000;
-// const TIME_STAMP_CODE: u8 = 0b0010;
-//
-// impl<'a> Message<'a, Ump> for UtilityMessage<'a> {
-//     fn data(&self) -> &'a [u32] {
-//         use UtilityMessage::*;
-//         match self {
-//             NoOp(m) => m.data(),
-//             TimeStamp(m) => m.data(),
-//         }
-//     }
-//     fn validate_data(data: &[u32]) -> Result<()> {
-//         match u8::from(data[0].nibble(2)) {
-//             NO_OP_CODE => NoOpMessage::validate_data(data),
-//             TIME_STAMP_CODE => TimeStampMessage::validate_data(data),
-//             _ => Err(Error::InvalidData),
-//         }
-//     }
-//     fn from_data_unchecked(data: &'a [u32]) -> Self {
-//         use UtilityMessage::*;
-//         match u8::from(data[0].nibble(2)) {
-//             NO_OP_CODE => NoOp(NoOpMessage::from_data_unchecked(data)),
-//             TIME_STAMP_CODE => TimeStamp(TimeStampMessage::from_data_unchecked(data)),
-//             _ => panic!(),
-//         }
-//     }
-// }
+pub enum UtilityBorrowed<'a> {
+    NoOp(NoOpBorrowed<'a>),
+    TimeStamp(TimeStampBorrowed<'a>),
+}
+
+pub enum UtilityOwned {
+    NoOp(NoOpOwned),
+    TimeStamp(TimeStampOwned),
+}
+
+const NO_OP_CODE: u8 = 0b0000;
+const TIME_STAMP_CODE: u8 = 0b0010;
+
+impl<'a> Data for UtilityBorrowed<'a> {
+    fn data(&self) -> &[u32] {
+        use UtilityBorrowed::*;
+        match self {
+            NoOp(m) => m.data(),
+            TimeStamp(m) => m.data(),
+        }
+    }
+}
+
+impl Data for UtilityOwned {
+    fn data(&self) -> &[u32] {
+        use UtilityOwned::*;
+        match self {
+            NoOp(m) => m.data(),
+            TimeStamp(m) => m.data(),
+        }
+    }
+}
+
+impl<'a> Grouped for UtilityBorrowed<'a> {
+    fn group(&self) -> u4 {
+        use UtilityBorrowed::*;
+        match self {
+            NoOp(m) => m.group(),
+            TimeStamp(m) => m.group(),
+        }
+    }
+}
+
+impl Grouped for UtilityOwned {
+    fn group(&self) -> u4 {
+        use UtilityOwned::*;
+        match self {
+            NoOp(m) => m.group(),
+            TimeStamp(m) => m.group(),
+        }
+    }
+}
+
+impl<'a> FromData<'a> for UtilityBorrowed<'a> {
+    fn validate_data(data: &[u32]) -> Result<()> {
+        match u8::from(data[0].nibble(2)) {
+            NO_OP_CODE => NoOpBorrowed::validate_data(data),
+            TIME_STAMP_CODE => TimeStampBorrowed::validate_data(data),
+            _ => Err(Error::InvalidData),
+        }
+    }
+    fn from_data_unchecked(data: &'a [u32]) -> Self {
+        use UtilityBorrowed::*;
+        match u8::from(data[0].nibble(2)) {
+            NO_OP_CODE => NoOp(NoOpBorrowed::from_data_unchecked(data)),
+            TIME_STAMP_CODE => TimeStamp(TimeStampBorrowed::from_data_unchecked(data)),
+            _ => panic!(),
+        }
+    }
+}
 
 pub fn validate_packet(p: &[u32], op_code: u4) -> Result<()> {
     if p.is_empty() {
