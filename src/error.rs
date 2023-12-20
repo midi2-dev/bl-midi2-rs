ea52f8e60d2e926edce8f1688c60c0c10f239488
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2023-12-20 14:09:17 +0100

    Feat: debug impl for error + std feature

--- src/error.rs
+++ src/error.rs
@@ -3,3 +3,13 @@ pub enum Error {
     BufferOverflow,
     InvalidData,
 }
+
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
+
+#[cfg(feature = "std")]
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        <Self as std::fmt::Debug>::fmt(self, f)
+    }
+}
