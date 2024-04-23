2c4476d879ae6686bb5e3aa81f566015be9cb4e8
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-04-23 15:34:34 +0200

    feat(hooks): update hooks
    * don't check cargo check or clippy on commit
    * use conventional-commit instead of committed
    * add commit template
    * check for merge conflict artefacts

--- configure-hooks.sh
+++ configure-hooks.sh
@@ -3,11 +3,13 @@
 install() {
     pre-commit install --install-hooks
     pre-commit install --install-hooks -t commit-msg
+    git config commit.template COMMIT_MESSAGE_TEMPLATE
 }
 
 uninstall() {
     pre-commit uninstall
     pre-commit uninstall -t commit-msg
+    git config --unset commit.template
 }
 
 case "$1" in
