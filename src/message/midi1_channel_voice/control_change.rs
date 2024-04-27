9d47f9e6b439abb467449e834069713e24e2f22b
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-04-27 07:43:15 +0200

    feat: migrate control change

--- src/message/midi1_channel_voice/control_change.rs
+++ src/message/midi1_channel_voice/control_change.rs
@@ -5,7 +5,7 @@ use crate::{
 
 const STATUS: u8 = 0b1011;
 
-#[midi2_proc::generate_message(FixedSize, MinSizeUmp(1), MinSizeBytes(2))]
+#[midi2_proc::generate_message(FixedSize, MinSizeUmp(1), MinSizeBytes(3))]
 struct ControlChange {
     #[property(common_properties::UmpMessageTypeProperty<UMP_MESSAGE_TYPE>)]
     ump_type: (),
@@ -20,86 +20,110 @@ struct ControlChange {
         schema::Bytes<0x00, 0x7F, 0x0>,
         schema::Ump<0x0000_7F00, 0x0, 0x0, 0x0>,
     >)]
-    pressure: crate::numeric_types::u7,
+    control: crate::numeric_types::u7,
+    #[property(common_properties::HybridSchemaProperty<
+        crate::numeric_types::u7,
+        schema::Bytes<0x00, 0x0, 0x7F>,
+        schema::Ump<0x0000_007F, 0x0, 0x0, 0x0>,
+    >)]
+    control_data: crate::numeric_types::u7,
 }
 
-// #[midi2_proc::generate_message(Grouped, Channeled)]
-// struct {
-//     ump_type: Property<
-//         NumericalConstant<MIDI1_CHANNEL_VOICE_TYPE>,
-//         UmpSchema<0xF000_0000, 0x0, 0x0, 0x0>,
-//         (),
-//     >,
-//     status: Property<
-//         NumericalConstant<OP_CODE>,
-//         UmpSchema<0x00F0_0000, 0x0, 0x0, 0x0>,
-//         BytesSchema<0xF0, 0x0, 0x0>,
-//     >,
-//     channel: Property<u4, UmpSchema<0x000F_0000, 0x0, 0x0, 0x0>, BytesSchema<0x0F, 0x0, 0x0>>,
-//     control: Property<u7, UmpSchema<0x0000_7F00, 0x0, 0x0, 0x0>, BytesSchema<0x0, 0x7F, 0x0>>,
-//     control_data: Property<u7, UmpSchema<0x0000_007F, 0x0, 0x0, 0x0>, BytesSchema<0x0, 0x0, 0x7F>>,
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        numeric_types::*,
+        traits::{Channeled, Grouped},
+    };
+    use pretty_assertions::assert_eq;
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use pretty_assertions::assert_eq;
-//
-//     #[test]
-//     fn builder() {
-//         assert_eq!(
-//             ControlChangeMessage::builder()
-//                 .group(u4::new(0xA))
-//                 .channel(u4::new(0x7))
-//                 .control(u7::new(0x36))
-//                 .control_data(u7::new(0x37))
-//                 .build(),
-//             Ok(ControlChangeMessage::Owned(ControlChangeOwned([
-//                 0x2AB7_3637,
-//                 0x0,
-//                 0x0,
-//                 0x0
-//             ]))),
-//         );
-//     }
-//
-//     #[test]
-//     fn group() {
-//         assert_eq!(
-//             ControlChangeMessage::from_data(&[0x2AB7_3637, 0x0, 0x0, 0x0])
-//                 .unwrap()
-//                 .group(),
-//             u4::new(0xA),
-//         );
-//     }
-//
-//     #[test]
-//     fn channel() {
-//         assert_eq!(
-//             ControlChangeMessage::from_data(&[0x2AB7_3637, 0x0, 0x0, 0x0])
-//                 .unwrap()
-//                 .channel(),
-//             u4::new(0x7),
-//         );
-//     }
-//
-//     #[test]
-//     fn control() {
-//         assert_eq!(
-//             ControlChangeMessage::from_data(&[0x2AB7_3637, 0x0, 0x0, 0x0])
-//                 .unwrap()
-//                 .control(),
-//             u7::new(0x36),
-//         );
-//     }
-//
-//     #[test]
-//     fn control_data() {
-//         assert_eq!(
-//             ControlChangeMessage::from_data(&[0x2AB7_3637, 0x0, 0x0, 0x0])
-//                 .unwrap()
-//                 .control_data(),
-//             u7::new(0x37),
-//         );
-//     }
-// }
+    #[test]
+    fn setters() {
+        let mut message = ControlChange::new_arr();
+        message.set_group(u4::new(0xA));
+        message.set_channel(u4::new(0x7));
+        message.set_control(u7::new(0x36));
+        message.set_control_data(u7::new(0x37));
+        assert_eq!(message, ControlChange([0x2AB7_3637,]));
+    }
+
+    #[test]
+    fn setters_bytes() {
+        let mut message = ControlChange::new_arr_bytes();
+        message.set_channel(u4::new(0x7));
+        message.set_control(u7::new(0x36));
+        message.set_control_data(u7::new(0x37));
+        assert_eq!(message, ControlChange([0xB7, 0x36, 0x37]));
+    }
+
+    #[test]
+    fn group() {
+        assert_eq!(
+            ControlChange::try_from(&[0x2AB7_3637_u32][..])
+                .unwrap()
+                .group(),
+            u4::new(0xA),
+        );
+    }
+
+    #[test]
+    fn channel() {
+        assert_eq!(
+            ControlChange::try_from(&[0x2AB7_3637_u32][..])
+                .unwrap()
+                .channel(),
+            u4::new(0x7),
+        );
+    }
+
+    #[test]
+    fn channel_bytes() {
+        assert_eq!(
+            ControlChange::try_from(&[0xB7_u8, 0x36_u8, 0x37_u8][..])
+                .unwrap()
+                .channel(),
+            u4::new(0x7),
+        );
+    }
+
+    #[test]
+    fn control() {
+        assert_eq!(
+            ControlChange::try_from(&[0x2AB7_3637_u32][..])
+                .unwrap()
+                .control(),
+            u7::new(0x36),
+        );
+    }
+
+    #[test]
+    fn control_bytes() {
+        assert_eq!(
+            ControlChange::try_from(&[0xB7_u8, 0x36_u8, 0x37_u8][..])
+                .unwrap()
+                .control(),
+            u7::new(0x36),
+        );
+    }
+
+    #[test]
+    fn control_data() {
+        assert_eq!(
+            ControlChange::try_from(&[0x2AB7_3637_u32][..])
+                .unwrap()
+                .control_data(),
+            u7::new(0x37),
+        );
+    }
+
+    #[test]
+    fn control_data_bytes() {
+        assert_eq!(
+            ControlChange::try_from(&[0xB7_u8, 0x36_u8, 0x37_u8][..])
+                .unwrap()
+                .control_data(),
+            u7::new(0x37),
+        );
+    }
+}
