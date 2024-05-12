7269e6be852d90e6804f835817273fe3a38d6968
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-05-12 06:43:02 +0200

    refactor: fix public exports of utility module

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,4 +1,4 @@
-pub mod no_op {
+mod no_op {
     use crate::detail::common_properties;
     use crate::utility;
     pub const STATUS: u8 = 0b0000;
@@ -12,7 +12,7 @@ pub mod no_op {
         time_data: u16,
     }
 }
-pub mod clock {
+mod clock {
     use crate::detail::common_properties;
     use crate::utility;
     pub const STATUS: u8 = 0b0001;
@@ -26,7 +26,7 @@ pub mod clock {
         time_data: u16,
     }
 }
-pub mod timestamp {
+mod timestamp {
     use crate::detail::common_properties;
     use crate::utility;
     pub const STATUS: u8 = 0b0010;
@@ -94,7 +94,7 @@ pub mod timestamp {
         }
     }
 }
-pub mod delta_clockstamp {
+mod delta_clockstamp {
     use crate::detail::common_properties;
     use crate::utility;
     pub const STATUS: u8 = 0b0100;
@@ -108,7 +108,7 @@ pub mod delta_clockstamp {
         time_data: u16,
     }
 }
-pub mod delta_clockstamp_tpq {
+mod delta_clockstamp_tpq {
     use crate::detail::common_properties;
     use crate::utility;
     pub const STATUS: u8 = 0b0011;
@@ -124,6 +124,12 @@ pub mod delta_clockstamp_tpq {
 }
 
 pub(crate) const UMP_MESSAGE_TYPE: u8 = 0x0;
+
+pub use no_op::NoOp;
+pub use clock::Clock;
+pub use timestamp::Timestamp;
+pub use delta_clockstamp::DeltaClockstamp;
+pub use delta_clockstamp_tpq::DeltaClockstampTPQ;
 
 struct DataProperty;
 
