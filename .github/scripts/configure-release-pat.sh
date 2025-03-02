4477393e868a5528e17a33e29b6e3c4056303162
ben.leadbetter <ben_leadbetter@hotmail.com>
2025-03-02 07:52:07 +0100

    chore: allow release action to finish releases

---
+++ .github/scripts/configure-release-pat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <PAT>"
+  exit 1
+fi
+
+PAT=$1
+
+git config --global user.email "ben_leadbetter@hotmail.com "
+git config --global user.name "GitHub Actions"
+git remote set-url origin https://x-access-token:${PAT}@https://github.com/midi2-dev/bl-midi2-rs
