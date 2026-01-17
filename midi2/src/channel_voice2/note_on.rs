8d49357a123e95213f6bd505ce942186c48a1081
Rahzael <rahzaelfoe@gmail.com>
2026-01-17 00:39:36 -0700

    feat: Implemented conversion from CV2 note on to CV1.

--- midi2/src/channel_voice2/note_on.rs
+++ midi2/src/channel_voice2/note_on.rs
@@ -7,6 +7,8 @@ use crate::{
     ux::{u4, u7},
 };
 
+use crate::traits::{Channeled, Grouped};
+use crate::{channel_voice1::NoteOn as NoteOn1, Data};
 pub(crate) const STATUS: u8 = 0b1001;
 
 /// MIDI 2.0 Channel Voice Note On Message
@@ -28,6 +30,21 @@ struct NoteOn {
     velocity: u16,
     #[property(AttributeProperty)]
     attribute: Option<Attribute>,
+}
+
+#[cfg(feature = "channel-voice1")]
+impl<const N: usize> Into<NoteOn1<[u32; N]>> for NoteOn<[u32; N]> {
+    fn into(self) -> NoteOn1<[u32; N]> {
+        let mut message = NoteOn1::<[u32; N]>::new();
+        message.set_group(self.group());
+        message.set_channel(self.channel());
+        message.set_note_number(self.note_number());
+        match self.velocity() {
+            0 => message.set_velocity(u7::new(0x01)),
+            _ => message.set_velocity(u7::new((self.velocity() >> 8).try_into().unwrap())),
+        }
+        message
+    }
 }
 
 #[cfg(test)]
@@ -95,5 +112,28 @@ mod tests {
                 .attribute(),
             Some(Attribute::Pitch7_9(Fixed7_9::from_bits(0b1110100110001010))),
         );
+    }
+
+    #[test]
+    fn into_midi_1() {
+        use crate::traits::{Channeled, Grouped};
+
+        let mut message2 = NoteOn::<[u32; 4]>::new();
+        message2.set_group(u4::new(0x8));
+        message2.set_channel(u4::new(0x8));
+        message2.set_note_number(u7::new(0x5E));
+        message2.set_velocity(0x6A14);
+
+        assert_eq!(message2, NoteOn([0x4898_5E00, 0x6A14_0000, 0x0, 0x0]),);
+
+        let mut message1 = NoteOn1::<[u32; 4]>::new();
+        message1.set_group(u4::new(0x8));
+        message1.set_channel(u4::new(0x8));
+        message1.set_note_number(u7::new(0x5E));
+        message1.set_velocity(u7::new(0x6A));
+
+        let message21: NoteOn1<[u32; 4]> = message2.into();
+
+        assert_eq!(message21, message1);
     }
 }
