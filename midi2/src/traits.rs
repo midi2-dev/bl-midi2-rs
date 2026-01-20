50fcc15646382bdc9a034aa22b277b182b58bb25
Rahzael <rahzaelfoe@gmail.com>
2026-01-20 01:29:02 -0700

    Feat: Added upscaling for all ranges but upper range and downscaling for lower range.

--- midi2/src/traits.rs
+++ midi2/src/traits.rs
@@ -731,3 +731,273 @@ pub(crate) trait BufferAccess<B: Buffer> {
     where
         B: BufferMut;
 }
+
+mod conversion {
+    pub(crate) trait Center:
+        Into<u32> + TryFrom<u32> + Sized + Copy + PartialEq + PartialOrd
+    where
+        <Self as TryFrom<u32>>::Error: core::fmt::Debug,
+    {
+        fn min_value() -> Self;
+        fn max_value() -> Self;
+
+        fn center_value() -> Self {
+            let min: u32 = Self::min_value().into();
+            let max: u32 = Self::max_value().into();
+
+            ((max - min) / 2_u32)
+                .try_into()
+                .expect("Center shouldn't be larger than max.")
+        }
+    }
+
+    impl Center for ux::u7 {
+        fn min_value() -> Self {
+            Self::min_value()
+        }
+        fn max_value() -> Self {
+            Self::max_value()
+        }
+    }
+
+    impl Center for ux::u9 {
+        fn min_value() -> Self {
+            Self::min_value()
+        }
+        fn max_value() -> Self {
+            Self::max_value()
+        }
+    }
+
+    impl Center for ux::u14 {
+        fn min_value() -> Self {
+            Self::min_value()
+        }
+        fn max_value() -> Self {
+            Self::max_value()
+        }
+    }
+
+    impl Center for u16 {
+        fn min_value() -> Self {
+            Self::min_value()
+        }
+        fn max_value() -> Self {
+            Self::max_value()
+        }
+    }
+
+    impl Center for u32 {
+        fn min_value() -> Self {
+            Self::min_value()
+        }
+        fn max_value() -> Self {
+            Self::max_value()
+        }
+    }
+
+    pub(crate) trait MinCenterMax: Center + core::ops::Shr<u32>
+    where
+        <Self as core::ops::Shr<u32>>::Output: Into<Self>,
+        <Self as TryFrom<u32>>::Error: core::fmt::Debug,
+    {
+        fn upscale<U: Center>(self) -> U
+        where
+            Self: Into<U>,
+            U: core::ops::Shl<u32>,
+            U: From<<U as core::ops::Shl<u32>>::Output>,
+            <U as TryFrom<u32>>::Error: core::fmt::Debug,
+        {
+            let min = Self::min_value();
+            let center = Self::center_value();
+            let max = Self::max_value();
+
+            match self {
+                s if s == min => U::min_value(),
+                s if s == max => U::max_value(),
+                s if s == center => U::center_value(),
+                s if (min..center).contains(&s) => {
+                    let self_max: u32 = Self::max_value().into();
+                    let other_max: u32 = U::max_value().into();
+                    let shift = (other_max - self_max).count_ones();
+                    let other: U = self.into();
+                    let upscaled = other << shift;
+                    upscaled.into()
+                }
+                s if (center..max).contains(&s) => s.into(),
+                _ => self.into(),
+            }
+        }
+
+        fn downscale<U: Center + TryFrom<Self>>(self) -> U
+        where
+            <U as TryFrom<u32>>::Error: core::fmt::Debug,
+            <U as TryFrom<Self>>::Error: core::fmt::Debug,
+        {
+            let self_max: u32 = Self::max_value().into();
+            let other_max: u32 = U::max_value().into();
+            let shift = (self_max - other_max).count_ones();
+            let downscaled = self >> shift;
+            let downscaled: Self = downscaled.into();
+
+            downscaled.try_into().expect("Downscaling should not fail.")
+        }
+    }
+
+    impl<U: Center + core::ops::Shr<u32>> MinCenterMax for U
+    where
+        <U as TryFrom<u32>>::Error: core::fmt::Debug,
+        <U as core::ops::Shr<u32>>::Output: Into<U>,
+    {
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_mins_upscaling() {
+            let min_u7 = ux::u7::new(0);
+            let min_u9 = ux::u9::new(0);
+            let min_u14 = ux::u14::new(0);
+            let min_u16 = 0_u16;
+            let min_u32 = 0_u32;
+
+            assert_eq!(min_u7.upscale::<ux::u7>(), min_u7);
+            assert_eq!(min_u7.upscale::<ux::u9>(), min_u9);
+            assert_eq!(min_u7.upscale::<ux::u14>(), min_u14);
+            assert_eq!(min_u7.upscale::<u16>(), min_u16);
+            assert_eq!(min_u7.upscale::<u32>(), min_u32);
+
+            assert_eq!(min_u9.upscale::<ux::u9>(), min_u9);
+            assert_eq!(min_u9.upscale::<ux::u14>(), min_u14);
+            assert_eq!(min_u9.upscale::<u16>(), min_u16);
+            assert_eq!(min_u9.upscale::<u32>(), min_u32);
+
+            assert_eq!(min_u14.upscale::<ux::u14>(), min_u14);
+            assert_eq!(min_u14.upscale::<u16>(), min_u16);
+            assert_eq!(min_u14.upscale::<u32>(), min_u32);
+
+            assert_eq!(min_u16.upscale::<u16>(), min_u16);
+            assert_eq!(min_u16.upscale::<u32>(), min_u32);
+
+            assert_eq!(min_u32.upscale::<u32>(), min_u32);
+        }
+
+        #[test]
+        fn test_maxs_upscaling() {
+            let max_u7 = ux::u7::new(127);
+            let max_u9 = ux::u9::new(511);
+            let max_u14 = ux::u14::new(16383);
+            let max_u16 = 0xFFFF_u16;
+            let max_u32 = 0xFFFFFFFF_u32;
+
+            assert_eq!(max_u7.upscale::<ux::u7>(), max_u7);
+            assert_eq!(max_u7.upscale::<ux::u9>(), max_u9);
+            assert_eq!(max_u7.upscale::<ux::u14>(), max_u14);
+            assert_eq!(max_u7.upscale::<u16>(), max_u16);
+            assert_eq!(max_u7.upscale::<u32>(), max_u32);
+
+            assert_eq!(max_u9.upscale::<ux::u9>(), max_u9);
+            assert_eq!(max_u9.upscale::<ux::u14>(), max_u14);
+            assert_eq!(max_u9.upscale::<u16>(), max_u16);
+            assert_eq!(max_u9.upscale::<u32>(), max_u32);
+
+            assert_eq!(max_u14.upscale::<ux::u14>(), max_u14);
+            assert_eq!(max_u14.upscale::<u16>(), max_u16);
+            assert_eq!(max_u14.upscale::<u32>(), max_u32);
+
+            assert_eq!(max_u16.upscale::<u16>(), max_u16);
+            assert_eq!(max_u16.upscale::<u32>(), max_u32);
+
+            assert_eq!(max_u32.upscale::<u32>(), max_u32);
+        }
+
+        #[test]
+        fn test_centers_upscaling() {
+            let center_u7 = ux::u7::new(63);
+            let center_u9 = ux::u9::new(255);
+            let center_u14 = ux::u14::new(8191);
+            let center_u16 = 0x7FFF_u16;
+            let center_u32 = 0x7FFFFFFF_u32;
+
+            assert_eq!(center_u7.upscale::<ux::u7>(), center_u7);
+            assert_eq!(center_u7.upscale::<ux::u9>(), center_u9);
+            assert_eq!(center_u7.upscale::<ux::u14>(), center_u14);
+            assert_eq!(center_u7.upscale::<u16>(), center_u16);
+            assert_eq!(center_u7.upscale::<u32>(), center_u32);
+
+            assert_eq!(center_u9.upscale::<ux::u9>(), center_u9);
+            assert_eq!(center_u9.upscale::<ux::u14>(), center_u14);
+            assert_eq!(center_u9.upscale::<u16>(), center_u16);
+            assert_eq!(center_u9.upscale::<u32>(), center_u32);
+
+            assert_eq!(center_u14.upscale::<ux::u14>(), center_u14);
+            assert_eq!(center_u14.upscale::<u16>(), center_u16);
+            assert_eq!(center_u14.upscale::<u32>(), center_u32);
+
+            assert_eq!(center_u16.upscale::<u16>(), center_u16);
+            assert_eq!(center_u16.upscale::<u32>(), center_u32);
+
+            assert_eq!(center_u32.upscale::<u32>(), center_u32);
+        }
+
+        #[test]
+        fn test_lower_range_downscaling() {
+            let lower_u7 = ux::u7::new(31);
+            let lower_u9 = ux::u9::new(127);
+            let lower_u14 = ux::u14::new(4095);
+            let lower_u16 = 0x3FFF_u16;
+            let lower_u32 = 0x3FFFFFFF_u32;
+
+            assert_eq!(lower_u32.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u32.downscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u32.downscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u32.downscale::<u16>(), lower_u16);
+            assert_eq!(lower_u32.downscale::<u32>(), lower_u32);
+
+            assert_eq!(lower_u16.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u16.downscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u16.downscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u16.downscale::<u16>(), lower_u16);
+
+            assert_eq!(lower_u14.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u14.downscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u14.downscale::<ux::u14>(), lower_u14);
+
+            assert_eq!(lower_u9.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u9.downscale::<ux::u9>(), lower_u9);
+
+            assert_eq!(lower_u7.downscale::<ux::u7>(), lower_u7);
+        }
+
+        #[test]
+        fn test_lower_range_upscaling() {
+            let lower_u7 = ux::u7::new(0x1F);
+            let lower_u9 = ux::u9::new(0x7C);
+            let lower_u14 = ux::u14::new(0xF80);
+            let lower_u16 = 0x3E00_u16;
+            let lower_u32 = 0x3E00_0000_u32;
+
+            assert_eq!(lower_u7.upscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u7.upscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u7.upscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u7.upscale::<u16>(), lower_u16);
+            assert_eq!(lower_u7.upscale::<u32>(), lower_u32);
+
+            assert_eq!(lower_u9.upscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u9.upscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u9.upscale::<u16>(), lower_u16);
+            assert_eq!(lower_u9.upscale::<u32>(), lower_u32);
+
+            assert_eq!(lower_u14.upscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u14.upscale::<u16>(), lower_u16);
+            assert_eq!(lower_u14.upscale::<u32>(), lower_u32);
+
+            assert_eq!(lower_u16.upscale::<u16>(), lower_u16);
+            assert_eq!(lower_u16.upscale::<u32>(), lower_u32);
+
+            assert_eq!(lower_u32.upscale::<u32>(), lower_u32);
+        }
+    }
+}
