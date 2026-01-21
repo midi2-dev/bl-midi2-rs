dc0a4a8811de8d28d9e2884b0426811ef3c17b8e
Rahzael <rahzaelfoe@gmail.com>
2026-01-20 23:42:52 -0700

    feat: Implemented mincentermax upscaling for upper range.

--- midi2/src/traits.rs
+++ midi2/src/traits.rs
@@ -801,10 +801,19 @@ mod conversion {
         <Self as core::ops::Shr<u32>>::Output: Into<Self>,
         <Self as TryFrom<u32>>::Error: core::fmt::Debug,
     {
-        fn upscale<U: Center>(self) -> U
+        fn upscale<
+            U: Center
+                + core::ops::Add<Output = U>
+                + core::ops::BitAnd<Output = U>
+                + core::fmt::Debug
+                + core::ops::BitOr<Output = U>,
+        >(
+            self,
+        ) -> U
         where
             Self: Into<U>,
-            U: core::ops::Shl<u32>,
+            U: core::ops::Shr<u32, Output = U>,
+            U: core::ops::Shl<u32, Output = U>,
             U: From<<U as core::ops::Shl<u32>>::Output>,
             <U as TryFrom<u32>>::Error: core::fmt::Debug,
         {
@@ -816,15 +825,47 @@ mod conversion {
                 s if s == min => U::min_value(),
                 s if s == max => U::max_value(),
                 s if s == center => U::center_value(),
+
                 s if (min..center).contains(&s) => {
                     let self_max: u32 = Self::max_value().into();
                     let other_max: u32 = U::max_value().into();
                     let shift = (other_max - self_max).count_ones();
                     let other: U = self.into();
                     let upscaled = other << shift;
-                    upscaled.into()
+                    upscaled
                 }
-                s if (center..max).contains(&s) => s.into(),
+
+                s if (center..max).contains(&s) => {
+                    let self_max: u32 = Self::max_value().into();
+                    let other_max: u32 = U::max_value().into();
+                    let self_bits = self_max.count_ones();
+                    let other_bits = other_max.count_ones();
+                    let shift = self_bits - 1;
+
+                    let initial: U = self.into();
+                    let initial_mask = U::max_value() >> (other_bits - self_bits + 1);
+                    let repeating = initial & initial_mask;
+
+                    let mut upscaled = initial;
+                    let mut remainder = other_bits - self_bits;
+
+                    while remainder > 0 {
+                        remainder = match remainder {
+                            remainder if remainder > shift => {
+                                upscaled = (upscaled << shift) | repeating;
+                                remainder - shift
+                            }
+                            _ => {
+                                let shift = shift - remainder;
+                                upscaled = (upscaled << remainder) | (repeating >> shift);
+                                0
+                            }
+                        };
+                    }
+                    std::dbg!(upscaled, initial, repeating);
+                    upscaled
+                }
+
                 _ => self.into(),
             }
         }
@@ -943,35 +984,6 @@ mod conversion {
         }
 
         #[test]
-        fn test_lower_range_downscaling() {
-            let lower_u7 = ux::u7::new(31);
-            let lower_u9 = ux::u9::new(127);
-            let lower_u14 = ux::u14::new(4095);
-            let lower_u16 = 0x3FFF_u16;
-            let lower_u32 = 0x3FFFFFFF_u32;
-
-            assert_eq!(lower_u32.downscale::<ux::u7>(), lower_u7);
-            assert_eq!(lower_u32.downscale::<ux::u9>(), lower_u9);
-            assert_eq!(lower_u32.downscale::<ux::u14>(), lower_u14);
-            assert_eq!(lower_u32.downscale::<u16>(), lower_u16);
-            assert_eq!(lower_u32.downscale::<u32>(), lower_u32);
-
-            assert_eq!(lower_u16.downscale::<ux::u7>(), lower_u7);
-            assert_eq!(lower_u16.downscale::<ux::u9>(), lower_u9);
-            assert_eq!(lower_u16.downscale::<ux::u14>(), lower_u14);
-            assert_eq!(lower_u16.downscale::<u16>(), lower_u16);
-
-            assert_eq!(lower_u14.downscale::<ux::u7>(), lower_u7);
-            assert_eq!(lower_u14.downscale::<ux::u9>(), lower_u9);
-            assert_eq!(lower_u14.downscale::<ux::u14>(), lower_u14);
-
-            assert_eq!(lower_u9.downscale::<ux::u7>(), lower_u7);
-            assert_eq!(lower_u9.downscale::<ux::u9>(), lower_u9);
-
-            assert_eq!(lower_u7.downscale::<ux::u7>(), lower_u7);
-        }
-
-        #[test]
         fn test_lower_range_upscaling() {
             let lower_u7 = ux::u7::new(0x1F);
             let lower_u9 = ux::u9::new(0x7C);
@@ -998,6 +1010,69 @@ mod conversion {
             assert_eq!(lower_u16.upscale::<u32>(), lower_u32);
 
             assert_eq!(lower_u32.upscale::<u32>(), lower_u32);
+        }
+
+        #[test]
+        fn test_upper_range_upscaling() {
+            // Test cases from MIDI 2.0 Bit Scaling and Resolution v1.0.2
+            let upper_u7_to_u16 = [
+                (ux::u7::new(0x46).upscale::<u16>(), 0x8C30_u16),
+                (ux::u7::new(0x60).upscale::<u16>(), 0xC104_u16),
+                (ux::u7::new(0x78).upscale::<u16>(), 0xF1C7_u16),
+            ];
+
+            let upper_u7_to_u32 = [
+                (ux::u7::new(0x46).upscale::<u32>(), 0x8C30C30C_u32),
+                (ux::u7::new(0x60).upscale::<u32>(), 0xC1041041_u32),
+                (ux::u7::new(0x78).upscale::<u32>(), 0xF1C71C71_u32),
+            ];
+
+            let upper_u16_to_u32 = [
+                (0x9C40_u16.upscale::<u32>(), 0x9C403880_u32),
+                (0xC000_u16.upscale::<u32>(), 0xC0008001_u32),
+                (0xFDE8_u16.upscale::<u32>(), 0xFDE8FBD1_u32),
+            ];
+
+            for (result, expected) in upper_u7_to_u16 {
+                assert_eq!(result, expected);
+            }
+
+            for (result, expected) in upper_u7_to_u32 {
+                assert_eq!(result, expected);
+            }
+
+            for (result, expected) in upper_u16_to_u32 {
+                assert_eq!(result, expected);
+            }
+        }
+
+        #[test]
+        fn test_lower_range_downscaling() {
+            let lower_u7 = ux::u7::new(31);
+            let lower_u9 = ux::u9::new(127);
+            let lower_u14 = ux::u14::new(4095);
+            let lower_u16 = 0x3FFF_u16;
+            let lower_u32 = 0x3FFFFFFF_u32;
+
+            assert_eq!(lower_u32.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u32.downscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u32.downscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u32.downscale::<u16>(), lower_u16);
+            assert_eq!(lower_u32.downscale::<u32>(), lower_u32);
+
+            assert_eq!(lower_u16.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u16.downscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u16.downscale::<ux::u14>(), lower_u14);
+            assert_eq!(lower_u16.downscale::<u16>(), lower_u16);
+
+            assert_eq!(lower_u14.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u14.downscale::<ux::u9>(), lower_u9);
+            assert_eq!(lower_u14.downscale::<ux::u14>(), lower_u14);
+
+            assert_eq!(lower_u9.downscale::<ux::u7>(), lower_u7);
+            assert_eq!(lower_u9.downscale::<ux::u9>(), lower_u9);
+
+            assert_eq!(lower_u7.downscale::<ux::u7>(), lower_u7);
         }
     }
 }
